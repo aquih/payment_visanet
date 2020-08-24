4b4b95a7434af7ef7f94f2f5ff1400173fda2aae
rocketgithub <rodrigo@solucionesprisma.com>
2020-08-24 12:01:35 -0600

    Para versión 11

--- controllers/payment.py
+++ controllers/payment.py
@@ -6,9 +6,9 @@ import werkzeug
 
 from odoo import http
 from odoo.http import request
+from odoo.release import version_info
 
 _logger = logging.getLogger(__name__)
-
 
 class VisaNetController(http.Controller):
     _return_url = '/payment/visanet/return'
@@ -21,4 +21,7 @@ class VisaNetController(http.Controller):
         _logger.info('VisaNet: entering form_feedback with post data %s', pprint.pformat(post))  # debug
         request.env['payment.transaction'].sudo().form_feedback(post, 'visanet')
         _logger.warn(post)
-        return werkzeug.utils.redirect(post.pop('return_url', '/payment/process'))
+        if version_info[0] > 11:
+            return werkzeug.utils.redirect(post.pop('return_url', '/payment/process'))
+        else:
+            return werkzeug.utils.redirect(post.pop('return_url', '/'))
