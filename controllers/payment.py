b08383342c570e4006c1eda586cbb337e6fa0f6a
rocketgithub <rodrigo@solucionesprisma.com>
2020-12-04 14:23:33 -0600

    Atributos correctos

--- controllers/payment.py
+++ controllers/payment.py
@@ -15,7 +15,7 @@ class VisaNetController(http.Controller):
 
     @http.route([
         '/payment/visanet/return',
-    ], type='http', auth='none', csrf=False)
+    ], type='http', auth='public', csrf=False)
     def visanet_return(self, **post):
         """ VisaNet """
         _logger.info('VisaNet: entering form_feedback with post data %s', pprint.pformat(post))  # debug
