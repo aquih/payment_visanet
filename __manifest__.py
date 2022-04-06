f965a093e75b31dd781ba25f4ddaad4cd0da1351
rocketgithub <rodrigo@aquih.com>
2022-04-06 11:10:18 -0600

    Ya no se usan

--- __manifest__.py
+++ __manifest__.py
@@ -16,6 +16,6 @@
     ],
     'images': ['static/description/icon.png'],
     'installable': True,
-    'post_init_hook': 'create_missing_journal_for_acquirers',
-    'uninstall_hook': 'uninstall_hook',
+#    'post_init_hook': 'create_missing_journal_for_acquirers',
+#    'uninstall_hook': 'uninstall_hook',
 }
