b22b23b9032141b56f10976d40bc3431d3099d85
rocketgithub <rodrigo@aquih.com>
2023-11-09 14:56:42 -0600

    Evitar errores cuando no existe recaptcha

--- static/src/js/payment_form.js
+++ static/src/js/payment_form.js
@@ -11,9 +11,11 @@ odoo.define('payment_visanet.payment_form', function (require) {
 
         willStart: function () {
             var recaptcha_v3_site_key = $('[name="recaptcha_v3_site_key"]').val();
-            return this._super.apply(this, arguments).then(function () {
-                return ajax.loadJS("https://www.google.com/recaptcha/api.js?render="+recaptcha_v3_site_key);
-            })
+            if (recaptcha_v3_site_key) {
+                return this._super.apply(this, arguments).then(function () {
+                    return ajax.loadJS("https://www.google.com/recaptcha/api.js?render="+recaptcha_v3_site_key);
+                })
+            }
         },
 
         //--------------------------------------------------------------------------
@@ -28,13 +30,17 @@ odoo.define('payment_visanet.payment_form', function (require) {
             var self = this;
             var sup = this._super;
             var args = arguments;
-            
-            ev.preventDefault();
-            grecaptcha.ready(function() {
-                grecaptcha.execute(recaptcha_v3_site_key, {action: 'submit'}).then(function(token) {
-                    return sup.apply(self, args);
+
+            if (recaptcha_v3_site_key) {
+                ev.preventDefault();
+                grecaptcha.ready(function() {
+                    grecaptcha.execute(recaptcha_v3_site_key, {action: 'submit'}).then(function(token) {
+                        return sup.apply(self, args);
+                    });
                 });
-            });
+            } else {
+                return sup.apply(self, args);
+            }
         },
     });
 });
