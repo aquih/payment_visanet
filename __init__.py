8d55be74b8a163f4fa2e279f789433b6e2186fb2
tono88 <estuarsandoval@gmail.com>
2024-04-02 10:55:24 -0600

    initial commit

--- __init__.py
+++ __init__.py
@@ -2,8 +2,15 @@
 
 from . import models
 from . import controllers
-from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
-from odoo.addons.payment import reset_payment_provider
+
+from odoo.addons.payment import setup_provider, reset_payment_provider
+
+
+def post_init_hook(cr, registry):
+    setup_provider(cr, registry, 'visanet')
+
 
 def uninstall_hook(cr, registry):
     reset_payment_provider(cr, registry, 'visanet')
+
+
