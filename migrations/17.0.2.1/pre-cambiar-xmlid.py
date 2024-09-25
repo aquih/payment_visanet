53295dcf8adeb5b787b116efdc628b6f5e032f12
rocketgithub <rodrigo@aquih.com>
2024-09-25 09:29:29 -0600

    Validar antes de renombrar

--- migrations/17.0.2.1/pre-cambiar-xmlid.py
+++ migrations/17.0.2.1/pre-cambiar-xmlid.py
@@ -5,5 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    util.records.rename_xmlid(cr, 'payment_visanet.payment_acquirer_visanet', 'payment_visanet.payment_provider_visanet')
-    _logger.info("Campos borrados")
+    env = util.env(cr)
+    if not self.env.ref('payment_visanet.payment_provider_visanet'):
+        util.records.rename_xmlid(cr, 'payment_visanet.payment_acquirer_visanet', 'payment_visanet.payment_provider_visanet')
+        _logger.info("xmlid renombrado")
